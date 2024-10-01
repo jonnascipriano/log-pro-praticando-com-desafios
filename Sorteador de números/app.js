7ecc337cd1e8ab2f587f1b8e5ee874654f3d956b
Jonnas Cipriano <jonnascipriano@gmail.com>
2024-10-01 20:59:01 -0300

    att

--- Sorteador de números/app.js
+++ Sorteador de números/app.js
@@ -6,36 +6,44 @@ function sortear(){
     let sorteados = []
     let numeros;
 
-    for(let i = 0; i < quantidade; i++){
-        numeros = obterNumeroAleatorio(de, ate)
-
-        while(sorteados.includes(numeros)){
-            numeros = obterNumeroAleatorio(de, ate)
-        }
-
-        sorteados.push(numeros)
-    }
     
-    let resultado = document.querySelector('#resultado')
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
-    alterarStatusBotao()
-}
-
-function obterNumeroAleatorio(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-function alterarStatusBotao(){
-    let botao = document.querySelector('#btn-reiniciar')
-    if (botao.classList.contains('container__botao-desabilitado')) {
-        botao.classList.remove('container__botao-desabilitado');
-        botao.classList.add('container__botao');
+    if(quantidade > ate){
+        alert('Quantidade maior que número final do sorteio. Digite valores válidos!')
+        document.querySelector('input#quantidade').value = ''
+        document.querySelector('input#de').value = ''
+        document.querySelector('input#ate').value = ''
+    } else if(de > ate){
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número"!')
+    } else if(quantidade > (ate - de + 1)){
+        alert('Quantidade superior ao intervalo de números escolhidos!')
+        return
+    } else {
+        for(let i = 0; i < quantidade; i++){
+        numeros = obterNumeroAleatorio(de, ate)
+        while (sorteados.includes(numeros)){
+            numeros = obterNumeroAleatorio(de,ate)
+        }
+        sorteados.push(numeros)
+        }
+        if(document.querySelector('#btn-reiniciar')){
+        document.querySelector('#btn-reiniciar').classList.remove('container__botao-desabilitado')
+        document.querySelector('#btn-reiniciar').classList.add('container__botao')
+        }
     }
+    console.log(sorteados)
 }
 
 function reiniciar(){
     document.querySelector('input#quantidade').value = ''
     document.querySelector('input#de').value = ''
     document.querySelector('input#ate').value = ''
-    document.querySelector('#resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`
+
+    if(document.querySelector('#btn-reiniciar')){
+        document.querySelector('#btn-reiniciar').classList.remove('container__botao')
+        document.querySelector('#btn-reiniciar').classList.add('container__botao-desabilitado')
+    }
+}
+
+function obterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
