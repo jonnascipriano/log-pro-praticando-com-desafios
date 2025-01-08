9bf68aee6da4312d0da6a7cfb205ef272f225c76
Jonnas Cipriano <jonnascipriano@gmail.com>
2025-01-08 20:46:52 -0300

    att

--- pasta-alura/amigo-secreto/js/app.js
+++ pasta-alura/amigo-secreto/js/app.js
@@ -1,16 +1,55 @@
-let nomes = []
+let amigos = [];
 
 function adicionar(){
-    let nomeAmigo = document.getElementById('nome-amigo').value;
-    for(let i = 0; i<=nomes.length; i++){
-        if(nomeAmigo === nomes[i]){
-            alert('Nome já existente')
-            break
-        } else {
-            nomes.push(nomeAmigo);
-            break
-        }
-    } 
+    let amigo = document.getElementById('nome-amigo')
+    if(amigo.value == ''){
+        alert('Informe o nome do amigo!')
+        return;
+    }
 
-    console.log(nomes)
+    if(amigos.includes(amigo.value)){
+        alert('Nome já adicionado!')
+        return;
+    }
+
+    let lista = document.getElementById('lista-amigos')
+    amigos.push(amigo.value);
+    if(lista.textContent == ''){
+        lista.textContent = amigo.value
+    } else {
+        lista.textContent = lista.textContent + ", " + amigo.value
+    }
+    amigo.value = ''
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function sortear() {
+
+    if(amigos.length < 4){
+        alert('Adicione pelo menos 4 amigos!')
+        return;
+    }
+
+    embaralhar(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
+        }
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
